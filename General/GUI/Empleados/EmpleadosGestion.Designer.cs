e50e5eaa7556033f8b442b30371a3fcbcbdd7c7c
mcnerio98 <ax.minck@gmail.com>
2020-12-06 21:31:30 -0600

    Ajuste empleados

--- General/GUI/Empleados/EmpleadosGestion.Designer.cs
+++ General/GUI/Empleados/EmpleadosGestion.Designer.cs
@@ -75,7 +75,7 @@
             this.toolStripLabel4});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1297, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(1221, 28);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -84,42 +84,42 @@
             this.btnAgregar.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAgregar.Image = global::General.Properties.Resources.add;
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(86, 22);
+            this.btnAgregar.Size = new System.Drawing.Size(113, 25);
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 28);
             // 
             // btnEditar
             // 
             this.btnEditar.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEditar.Image = global::General.Properties.Resources.edit;
             this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(66, 22);
+            this.btnEditar.Size = new System.Drawing.Size(87, 25);
             this.btnEditar.Text = "Editar";
             this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 28);
             // 
             // btnEliminar
             // 
             this.btnEliminar.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEliminar.Image = global::General.Properties.Resources.delete;
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(79, 22);
+            this.btnEliminar.Size = new System.Drawing.Size(102, 25);
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // toolStripSeparator3
             // 
             this.toolStripSeparator3.Name = "toolStripSeparator3";
-            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 28);
             // 
             // txbFiltrar
             // 
@@ -127,7 +127,7 @@
             this.txbFiltrar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txbFiltrar.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txbFiltrar.Name = "txbFiltrar";
-            this.txbFiltrar.Size = new System.Drawing.Size(150, 25);
+            this.txbFiltrar.Size = new System.Drawing.Size(199, 28);
             this.txbFiltrar.TextChanged += new System.EventHandler(this.txbFiltrar_TextChanged);
             // 
             // toolStripLabel4
@@ -136,16 +136,18 @@
             this.toolStripLabel4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.toolStripLabel4.Image = global::General.Properties.Resources.lupa;
             this.toolStripLabel4.Name = "toolStripLabel4";
-            this.toolStripLabel4.Size = new System.Drawing.Size(16, 22);
+            this.toolStripLabel4.Size = new System.Drawing.Size(25, 25);
             this.toolStripLabel4.Text = "toolStripLabel4";
             // 
             // statusStrip1
             // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.lblRegistros});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 456);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 561);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(1297, 22);
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1221, 27);
             this.statusStrip1.TabIndex = 1;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -153,7 +155,7 @@
             // 
             this.lblRegistros.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblRegistros.Name = "lblRegistros";
-            this.lblRegistros.Size = new System.Drawing.Size(160, 17);
+            this.lblRegistros.Size = new System.Drawing.Size(206, 21);
             this.lblRegistros.Text = "0 Registros Encontrados";
             // 
             // dtgDatos
@@ -189,19 +191,22 @@
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
             this.dtgDatos.DefaultCellStyle = dataGridViewCellStyle2;
             this.dtgDatos.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dtgDatos.Location = new System.Drawing.Point(0, 25);
+            this.dtgDatos.Location = new System.Drawing.Point(0, 28);
+            this.dtgDatos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtgDatos.MultiSelect = false;
             this.dtgDatos.Name = "dtgDatos";
             this.dtgDatos.ReadOnly = true;
             this.dtgDatos.RowHeadersVisible = false;
+            this.dtgDatos.RowHeadersWidth = 51;
             this.dtgDatos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dtgDatos.Size = new System.Drawing.Size(1297, 431);
+            this.dtgDatos.Size = new System.Drawing.Size(1221, 533);
             this.dtgDatos.TabIndex = 2;
             // 
             // DUI
             // 
             this.DUI.DataPropertyName = "DUI";
             this.DUI.HeaderText = "DUI";
+            this.DUI.MinimumWidth = 6;
             this.DUI.Name = "DUI";
             this.DUI.ReadOnly = true;
             this.DUI.Width = 125;
@@ -210,6 +215,7 @@
             // 
             this.Nombres.DataPropertyName = "Nombres";
             this.Nombres.HeaderText = "Nombres";
+            this.Nombres.MinimumWidth = 6;
             this.Nombres.Name = "Nombres";
             this.Nombres.ReadOnly = true;
             this.Nombres.Width = 150;
@@ -218,6 +224,7 @@
             // 
             this.Apellidos.DataPropertyName = "Apellidos";
             this.Apellidos.HeaderText = "Apellidos";
+            this.Apellidos.MinimumWidth = 6;
             this.Apellidos.Name = "Apellidos";
             this.Apellidos.ReadOnly = true;
             this.Apellidos.Width = 150;
@@ -226,6 +233,7 @@
             // 
             this.Direccion.DataPropertyName = "Direccion";
             this.Direccion.HeaderText = "Direccion";
+            this.Direccion.MinimumWidth = 6;
             this.Direccion.Name = "Direccion";
             this.Direccion.ReadOnly = true;
             this.Direccion.Width = 175;
@@ -234,20 +242,25 @@
             // 
             this.Telefono.DataPropertyName = "Telefono";
             this.Telefono.HeaderText = "Telefono";
+            this.Telefono.MinimumWidth = 6;
             this.Telefono.Name = "Telefono";
             this.Telefono.ReadOnly = true;
+            this.Telefono.Width = 125;
             // 
             // Genero
             // 
             this.Genero.DataPropertyName = "Genero";
             this.Genero.HeaderText = "Genero";
+            this.Genero.MinimumWidth = 6;
             this.Genero.Name = "Genero";
             this.Genero.ReadOnly = true;
+            this.Genero.Width = 125;
             // 
             // FechaNacimiento
             // 
             this.FechaNacimiento.DataPropertyName = "FechaNacimiento";
-            this.FechaNacimiento.HeaderText = "FechaNacimiento";
+            this.FechaNacimiento.HeaderText = "F.Nacimiento";
+            this.FechaNacimiento.MinimumWidth = 6;
             this.FechaNacimiento.Name = "FechaNacimiento";
             this.FechaNacimiento.ReadOnly = true;
             this.FechaNacimiento.Width = 120;
@@ -256,49 +269,60 @@
             // 
             this.Usuario.DataPropertyName = "Usuario";
             this.Usuario.HeaderText = "Usuario";
+            this.Usuario.MinimumWidth = 6;
             this.Usuario.Name = "Usuario";
             this.Usuario.ReadOnly = true;
+            this.Usuario.Width = 125;
             // 
             // Rol
             // 
             this.Rol.DataPropertyName = "Rol";
             this.Rol.HeaderText = "Rol";
+            this.Rol.MinimumWidth = 6;
             this.Rol.Name = "Rol";
             this.Rol.ReadOnly = true;
+            this.Rol.Width = 125;
             // 
             // Descripcion
             // 
             this.Descripcion.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.Descripcion.DataPropertyName = "Descripcion";
             this.Descripcion.HeaderText = "Descripcion";
+            this.Descripcion.MinimumWidth = 6;
             this.Descripcion.Name = "Descripcion";
             this.Descripcion.ReadOnly = true;
+            this.Descripcion.Visible = false;
             // 
             // IDRol
             // 
             this.IDRol.DataPropertyName = "IDRol";
             this.IDRol.HeaderText = "IDRol";
+            this.IDRol.MinimumWidth = 6;
             this.IDRol.Name = "IDRol";
             this.IDRol.ReadOnly = true;
             this.IDRol.Visible = false;
+            this.IDRol.Width = 125;
             // 
             // IDUsuario
             // 
             this.IDUsuario.DataPropertyName = "IDUsuario";
             this.IDUsuario.HeaderText = "IDUsuario";
+            this.IDUsuario.MinimumWidth = 6;
             this.IDUsuario.Name = "IDUsuario";
             this.IDUsuario.ReadOnly = true;
             this.IDUsuario.Visible = false;
+            this.IDUsuario.Width = 125;
             // 
             // EmpleadosGestion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1297, 478);
+            this.ClientSize = new System.Drawing.Size(1221, 588);
             this.Controls.Add(this.dtgDatos);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "EmpleadosGestion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Gestión  Empleados";
